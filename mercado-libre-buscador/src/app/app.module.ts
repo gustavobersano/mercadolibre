1295323f0411909b2d2b1886ce633ed52e8d5c00
Gustavo <gustavo.bersano@gmail.com>
2019-05-19 14:42:45 -0300

    Se instala Angular Material Desing.

--- mercado-libre-buscador/src/app/app.module.ts
+++ mercado-libre-buscador/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 // Routing Module
 import { AppRoutingModule } from './app-routing.module';
+// Material
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 // Components
 import { AppComponent } from './app.component';
 import { SearchingBarComponent } from './components/searching-bar/searching-bar.component';
@@ -15,6 +17,7 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
