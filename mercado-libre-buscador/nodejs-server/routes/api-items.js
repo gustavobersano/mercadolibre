d873206d0dcc960e87d17b9e03d0e441a46b8444
gbersano <gbersano@grupoassa.com>
2019-05-24 01:05:47 -0300

    Se corrige servicio by query en el back.

--- mercado-libre-buscador/nodejs-server/routes/api-items.js
+++ mercado-libre-buscador/nodejs-server/routes/api-items.js
@@ -3,57 +3,32 @@ let router = express.Router();
 let Item = require('../models/item.js');
 let request = require('request');
 
-const urlBase = 'https://api.mercadolibre.com';
-//var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const urlBase = 'https://api.mercadolibre.com/';
 
-
-/* GET ALL ITEMS */
+/* GET ITEMS BY QUERY */
 router.get('/items', function (req, res, next) {
-  //const path = '/sites/MLA/search'
-  //let urlComplete = urlBase.concat(path).concat('?q=').concat(req.query.q);
-  //let urlComplete = urlBase + path + '?q=' + req.query.q;
-  let theUrl = "https://api.mercadolibre.com/sites/MLA/search?q=:mesa";
-  //let theUrl = "https://api.mercadolibre.com/sites/MLA/search?q=:mesa";
-             //"https://api.mercadolibre.com/sites/MLA/search?q=​:mesa"
-            //"https://api.mercadolibre.com/sites/MLA/search?q=​:mesa"
-  //let theUrl = "https://api.mercadolibre.com/sites/MLA/search?q=" + req.query.q;
 
-  //var theUrl = "https://api.mercadolibre.com/items/MLA670899102";
-  // Para obtener las categorias https://api.mercadolibre.com/sites/MLA/
-  /*
-/categories X
-  */
- // https://developers.mercadolibre.com.ar/es_ar/usuarios-y-aplicaciones/
+  const myURL = new URL(urlBase + "sites/MLA/search?q=" + req.query.q);
 
-  request(theUrl, function (error, response, body) {
-    res.json(
-      {
-        body: JSON.parse(body)
-        //query: req.query.q,
-        //url: urlComplete
-      }
-      
-    );
+  request(myURL.toString(), function (error, response, body) {
+    res.json({
+      body: JSON.parse(body),
+    });
   });
-/*
-  res.json(
-  {
-    test: 'test'
-  });
-*/
 });
 
 /* GET SINGLE ITEM BY ID */
 router.get('/:id', function (req, res, next) {
 
   //req.params.id
+  //var theUrl = "https://api.mercadolibre.com/items/MLA670899102";
 
-
-
-  Item.findById(req.params.id, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
 });
 
 module.exports = router;
+
+// Para obtener las categorias https://api.mercadolibre.com/sites/MLA/
+  /*
+/categories X
+  */
+  // https://developers.mercadolibre.com.ar/es_ar/usuarios-y-aplicaciones/
