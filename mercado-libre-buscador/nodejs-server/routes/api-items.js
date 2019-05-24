7a8b58a44da809adb15c8bbcc1c0aa7d0c8cc005
gbersano <gbersano@grupoassa.com>
2019-05-23 22:51:47 -0300

    Se agrega llamado fijo a la api de ML.

--- mercado-libre-buscador/nodejs-server/routes/api-items.js
+++ mercado-libre-buscador/nodejs-server/routes/api-items.js
@@ -12,7 +12,7 @@ router.get('/items', function (req, res, next) {
   //const path = '/sites/MLA/search'
   //let urlComplete = urlBase.concat(path).concat('?q=').concat(req.query.q);
   //let urlComplete = urlBase + path + '?q=' + req.query.q;
-  //let theUrl = "https://api.mercadolibre.com/sites/MLA/search?q=%E2%80%8B:mesa";
+  let theUrl = "https://api.mercadolibre.com/sites/MLA/search?q=:mesa";
   //let theUrl = "https://api.mercadolibre.com/sites/MLA/search?q=:mesa";
              //"https://api.mercadolibre.com/sites/MLA/search?q=​:mesa"
             //"https://api.mercadolibre.com/sites/MLA/search?q=​:mesa"
@@ -24,22 +24,23 @@ router.get('/items', function (req, res, next) {
 /categories X
   */
  // https://developers.mercadolibre.com.ar/es_ar/usuarios-y-aplicaciones/
-  /*
+
   request(theUrl, function (error, response, body) {
     res.json(
       {
-        body: JSON.parse(body),
-        query: req.query.q,
-        url: urlComplete
+        body: JSON.parse(body)
+        //query: req.query.q,
+        //url: urlComplete
       }
       
     );
   });
-  */
+/*
   res.json(
   {
     test: 'test'
   });
+*/
 });
 
 /* GET SINGLE ITEM BY ID */
